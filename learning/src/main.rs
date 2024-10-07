02e59f79d76f195fb07ea13cd4603e30673da558
Marty <marty@kuori.cz>
2024-10-07 16:57:37 +0200

    Refactor main.rs to extract and print the first word of a sentence

--- learning/src/main.rs
+++ learning/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let sentence = String::from("penys quick brown fox jumps over the lazy dog");
+    let first_word = get_first_word(sentence);
+    println!("First word: {}", first_word);
+}
+
+fn get_first_word(s: String) -> String {
+    let mut first_word = String::new();
+    for char in s.chars() {
+        first_word.push_str(char.to_string().as_str());
+        if char == ' ' {
+            break;
+        }
+    }
+    return first_word;
 }
